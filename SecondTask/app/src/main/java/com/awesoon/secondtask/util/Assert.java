7416ba3151d7afafc46f6e97386304527cbf2a00
Andrew Kuchev <0coming.soon@gmail.com>
2017-04-17 13:37:20 +0500

    Added comments Added test for math utils

--- SecondTask/app/src/main/java/com/awesoon/secondtask/util/Assert.java
+++ SecondTask/app/src/main/java/com/awesoon/secondtask/util/Assert.java
@@ -4,12 +4,26 @@ public final class Assert {
   private Assert() {
   }
 
+  /**
+   * Ensures that the given value is not null. If the value is null, throws a NullPointerException.
+   *
+   * @param value   A value to check.
+   * @param message An exception message.
+   * @throws NullPointerException if the given value is null
+   */
   public static void notNull(Object value, String message) {
     if (value == null) {
       throw new NullPointerException(message);
     }
   }
 
+  /**
+   * Ensures that the given condition is true. If the condition is false, throws an AssertionError.
+   *
+   * @param condition A condition to check.
+   * @param message   An exception message.
+   * @throws AssertionError if the given condition is false.
+   */
   public static void isTrue(boolean condition, String message) {
     if (!condition) {
       throw new AssertionError(message);
